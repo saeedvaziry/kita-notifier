3d1b048979675f07bc19c2c74f81c8970c03bc52
Saeed Vaziry <saeed.vaziry@metro-markets.de>
2022-11-16 00:07:57 +0100

    upgrade

--- notifier.py
+++ notifier.py
@@ -20,24 +20,20 @@ with requests.Session() as s:
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) '
                       'Chrome/51.0.2704.103 Safari/537.36 '
     }
-    s.get(os.environ.get('BASE_URL') + '/login/')
-    login = s.post(os.environ.get('BASE_URL') + '/login/', data={
-        'str_email': os.environ.get('EMAIL'),
-        'str_passwort': os.environ.get('PASSWORD'),
-        'act_login': '',
+    token = s.get(os.environ.get('BASE_URL') + '/login/')
+    token_soup = BeautifulSoup(token.content.decode('utf-8'), 'html.parser')
+    token_el = token_soup.find_all('meta', attrs={'name': 'csrf-token'})
+    csrf_token = token_el[0]['content']
+    login = s.post(os.environ.get('BASE_URL') + '/login', data={
+        'email': os.environ.get('EMAIL'),
+        'password': os.environ.get('PASSWORD'),
+        '_token': csrf_token
     })
-    kitas = s.get(os.environ.get('BASE_URL') + '/login/startseite/')
-    soup = BeautifulSoup(kitas.content.decode('utf-8'), 'html.parser')
-    table = soup.find_all('table', attrs={'class': 'contenttable stackable'})
-    match = False
+    soup = BeautifulSoup(login.content.decode('utf-8'), 'html.parser')
+    table = soup.find_all('i', attrs={'class': 'fal fa-user-friends'})
+    print(table[0])
     msg = ''
     for t in table:
-        rows = t.findChildren('tr')
-        for r in rows:
-            columns = r.findChildren('td')
-            for i, c in enumerate(columns):
-                if i == 1:
-                    value = "".join(line.strip() for line in c.string.split("\n"))
-                    msg += value + "\n"
+        msg += t.parent.text.strip() + "\n"
 
     notify(msg)
